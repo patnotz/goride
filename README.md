6971c01c1f872bfe8b21a84ffc0743910a2bff2e
Pat Notz <patnotz@gmail.com>
2020-09-28 19:16:57 -0700

    Start a basic tls web server

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Seriously, goride
+
+## Generating the SSL cert and private key
+
+Generate the key:
+
+'''
+openssl req -new -newkey rsa:2048 -nodes -keyout goride.key -out goride.csr
+'''
+
+Self-sign it:
+
+'''
+openssl x509 -req -days 365 -in goride.csr -signkey goride.key -out goride.crt
+'''
+
+Install the cert in Chrome: chrome://settings/certificates
+
+
+## TODO List
+
+1. Figure out OAuth authentication with Strava
+1. Unmarshal JSON data using a struct to select the fields we want. See https://medium.com/rungo/working-with-json-in-go-7e3a37c5a07b
--- goride.go
+++ goride.go
@@ -2,12 +2,18 @@
 // Click here and start typing.
 package main
 
-import "encoding/json"
-import "fmt"
-import "io/ioutil"
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func err_handler(err error) {
+const base_url = "https://www.strava.com/api/v3/"
+const access_token = "355aabb46aa2840403a73472e01f4421f946659f"
+
+func errHandler(err error) {
 	if err != nil {
 		fmt.Println("Error :(")
 		fmt.Println(err)
@@ -15,46 +21,58 @@ func err_handler(err error) {
 	}
 }
 
-func make_request(url string) (dat map[string]interface{}) {
-	access_token := "7a69d16769e7791d56aabff25268f8800ce923d7"
+func makeRequest(url string) (dat map[string]interface{}) {
 	fmt.Println("fetching: " + url)
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
-	err_handler(err)
+	errHandler(err)
 	req.Header.Add("Authorization", ("Bearer " + access_token))
 	req.Header.Add("activity", "read")
 	resp, err := client.Do(req)
-	err_handler(err)
+	errHandler(err)
 
 	defer resp.Body.Close()
 	body_bytes, err := ioutil.ReadAll(resp.Body)
-	err_handler(err)
+	errHandler(err)
 
 	err = json.Unmarshal(body_bytes, &dat)
-	err_handler(err)
+	errHandler(err)
 	return
 }
 
-func main() {
-	
-	base_url := "https://www.strava.com/api/v3/"
-
+func getAthleteData() (username string, athlete_id string) {
 	// Get Athlete data
 	fmt.Println("Getting athlete data...")
 	url := base_url + "athlete"
-	dat := make_request(url)
+	dat := makeRequest(url)
 	fmt.Println(dat)
 
 	// Unpack Athlete data
-	athlete_id := fmt.Sprintf("%.0f", dat["id"].(float64))
-	username := dat["username"].(string)
+	athlete_id = fmt.Sprintf("%.0f", dat["id"].(float64))
+	username = dat["username"].(string)
 	fmt.Println("Username  : " + username)
 	fmt.Println("Athlete ID: " + athlete_id)
+	return
+}
+
+func getActivityData() {
+	username, athlete_id := getAthleteData()
 
 	// Get Activity data
 	fmt.Println("Getting activity data for " + username + " (" + athlete_id + ")")
-	url = base_url + "athlete/activities"
-	dat = make_request(url)
+	url := base_url + "athlete/activities"
+	dat := makeRequest(url)
 	fmt.Println(dat)
-	
+}
+
+func main() {
+
+	// handle '/' route
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		username, athlete_id := getAthleteData()
+		fmt.Fprint(res, "Go Ride, " + username + " (" + athlete_id + ")!")
+	})
+
+	// run the server on port 9000
+	log.Fatal(http.ListenAndServeTLS(":9000", "goride.crt", "goride.key", nil))
 }
